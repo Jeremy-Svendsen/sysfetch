3a362b7419a51af3d15089e93ce3a1db5a592d99
wick3dr0se <wick3dr0se@protonmail.com>
2021-12-22 16:32:14 -0600

    mv variables; rmv unecessary string manipulation

--- gpu-nix.sh
+++ gpu-nix.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
 # this script search '41' in /proc/bus/pci/devices for vendor and device id, then scrapes https://pci-ids.ucw.cz using taken id's for GPU output
-
-html_strip="s|</b>|-|g;s|<[^>]*>||g"
 while read -r line ; do
 	case $line in
-		*41*) id=${line:4} ;;
+		*41*) id=$line ;;
 	esac
 done < /proc/bus/pci/devices
 id=$(cut -f2 <<< $id)
 vendor_id=${id::-4}
 device_id=${id:4}
 
-pci_id=$(curl https://pci-ids.ucw.cz/read/PC/$vendor_id 2>/dev/null)
+pci_data=$(curl https://pci-ids.ucw.cz/read/PC/$vendor_id 2>/dev/null)
 while read -r line ; do
 	case $line in
 		*'="nam'*) gpu_vendor=$line ;;
 	esac
-done <<< $pci_id
+done <<< $pci_data
+html_strip="s|</b>|-|g;s|<[^>]*>||g"
 gpu_vendor=$(sed "$html_strip" <<< $gpu_vendor)
 gpu_vendor=${gpu_vendor//Name: }
 gpu_vendor=${gpu_vendor//Note:*}
@@ -26,7 +25,7 @@ while read -r line ; do
 	case $line in
 		*$device_id*) gpu_name=$line ;;
 	esac
-done <<< $pci_id
+done <<< $pci_data
 
 gpu_name=$(sed "$html_strip" <<< $gpu_name)
 gpu_name=${gpu_name:4}
