a6e8a664565ff48a06bc88f310c8f92a67024276
wick3dr0se <wick3dr0se@protonmail.com>
2021-12-26 11:18:14 -0600

    search for 0000c instead of 41

--- gpu-nix.sh
+++ gpu-nix.sh
@@ -3,7 +3,7 @@
 # this script search '41' in /proc/bus/pci/devices for vendor and device id, then scrapes https://pci-ids.ucw.cz using taken id's for GPU output
 while read -r line ; do
 	case $line in
-		*41*) id=$line ;;
+		*0000c*) id=$line ;;
 	esac
 done < /proc/bus/pci/devices
 id=$(cut -f2 <<< $id)
