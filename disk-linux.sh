0b3fe7d18777ffe48fdbc31b38e056346d276eb7
wick3dr0se <wick3dr0se@protonmail.com>
2022-01-14 17:51:28 -0600

    rmv disk & cpu temp scripts

--- disk-linux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-# We need a proper regex to strip this in case disk block path is like sda, sdb, sdc, nvme0n1p1, or some other. Current strip will break for nvme drives 
-disk_strip="s/SSD//;s/[0-9$]*GB//;s/ *$//"
-disk_path=$(df / | awk -F ' ' 'FNR==2 {print $1}')
-disk_model=$(lsblk $disk_path -io MODEL | sed -n 2p | sed "$disk_strip")
